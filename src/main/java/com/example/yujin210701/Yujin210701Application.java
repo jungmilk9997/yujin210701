f6f7193a38ee41a4833960425201f8a4d83f524f
jungmilk9997 <jungmilk9997@naver.com>
2021-08-30 14:13:55 +0900

    Initial commit

---
+++ src/main/java/com/example/yujin210701/Yujin210701Application.java
@@ -0,0 +1,13 @@
+package com.example.yujin210701;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yujin210701Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yujin210701Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yujin210701/Yujin210701ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yujin210701;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yujin210701ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
